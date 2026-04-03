d2d42b4c13c1eeb53c3617dbc35102868b4066f1
Benjamin Bruarøy <openclawbruker@Benjamin-sin-Mac-mini.local>
2026-04-03 11:32:55 +0200

    add Google Tag Manager (GTM-PTRQBMM7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,17 @@ export default function RootLayout({
       className={`${inter.variable} ${spaceGrotesk.variable} ${jetbrainsMono.variable} h-full antialiased`}
     >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PTRQBMM7');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         {supabaseUrl && (
           <>
             <link rel="dns-prefetch" href={supabaseUrl} />
@@ -72,6 +83,16 @@ export default function RootLayout({
         )}
       </head>
       <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PTRQBMM7"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:bg-primary focus:text-white focus:px-4 focus:py-2 focus:rounded"
